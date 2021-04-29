0799fb70815479e00b5127a519fc721631b334f3
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-04-30 02:39:43 +0900

    feat: 공통 에러 코드 추가

---
+++ src/main/java/com/lubycon/curriculum/base/error/ErrorCode.java
@@ -0,0 +1,25 @@
+package com.lubycon.curriculum.base.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+  // Common
+  INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "C001", "Invalid Input Value"),
+  METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C002", "Invalid Input Value"),
+  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C003", "Internal Server Error"),
+  INVALID_TYPE_VALUE(HttpStatus.BAD_REQUEST, "C004", "Invalid Type Value"),
+  ENTITY_NOT_FOUND(HttpStatus.BAD_REQUEST, "C005", "Entity Not Found"),
+  FAIL_ENUM_BINDING(HttpStatus.BAD_REQUEST, "C006", "Fail Enum Binding");
+  
+  private final String code;
+  private final String message;
+  private final HttpStatus status;
+
+  ErrorCode(final HttpStatus status, final String code, final String message) {
+    this.status = status;
+    this.message = message;
+    this.code = code;
+  }
+}
--- src/main/java/com/lubycon/curriculum/base/error/ErrorResponse.java
+++ src/main/java/com/lubycon/curriculum/base/error/ErrorResponse.java
@@ -2,9 +2,12 @@ package com.lubycon.curriculum.base.error;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -13,6 +16,39 @@ public class ErrorResponse {
   private String message;
   private List<FieldError> errors;
   private String code;
+
+
+  private ErrorResponse(final ErrorCode code, final List<FieldError> errors) {
+    this.message = code.getMessage();
+    this.errors = errors;
+    this.code = code.getCode();
+  }
+
+  private ErrorResponse(final ErrorCode code) {
+    this.message = code.getMessage();
+    this.code = code.getCode();
+    this.errors = new ArrayList<>();
+  }
+
+
+  public static ErrorResponse of(final ErrorCode code, final BindingResult bindingResult) {
+    return new ErrorResponse(code, FieldError.of(bindingResult));
+  }
+
+  public static ErrorResponse of(final ErrorCode code) {
+    return new ErrorResponse(code);
+  }
+
+  public static ErrorResponse of(final ErrorCode code, final List<FieldError> errors) {
+    return new ErrorResponse(code, errors);
+  }
+
+  public static ErrorResponse of(MethodArgumentTypeMismatchException e) {
+    final String value = e.getValue() == null ? "" : e.getValue().toString();
+    final List<ErrorResponse.FieldError> errors = ErrorResponse.FieldError
+        .of(e.getName(), value, e.getErrorCode());
+    return new ErrorResponse(ErrorCode.INVALID_TYPE_VALUE, errors);
+  }
 
   @Getter
   @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -32,6 +68,17 @@ public class ErrorResponse {
       List<FieldError> fieldErrors = new ArrayList<>();
       fieldErrors.add(new FieldError(field, value, reason));
       return fieldErrors;
+    }
+
+    private static List<FieldError> of(final BindingResult bindingResult) {
+      final List<org.springframework.validation.FieldError> fieldErrors = bindingResult
+          .getFieldErrors();
+      return fieldErrors.stream()
+          .map(error -> new FieldError(
+              error.getField(),
+              error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+              error.getDefaultMessage()))
+          .collect(Collectors.toList());
     }
   }
 }
