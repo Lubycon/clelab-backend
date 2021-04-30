3e3a48300745f6ec4b57bfe792fa6d6ec4f1db10
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-05-01 00:01:25 +0900

    feat: 커리큘럼 리스트 가져오는 서비스의 추가

--- src/main/java/com/lubycon/curriculum/curriculum/service/CurriculumService.java
+++ src/main/java/com/lubycon/curriculum/curriculum/service/CurriculumService.java
@@ -1,8 +1,10 @@
 package com.lubycon.curriculum.curriculum.service;
 
+import com.lubycon.curriculum.curriculum.domain.Curriculum;
 import com.lubycon.curriculum.curriculum.dto.CurriculumResponse;
-import java.util.Collections;
+import com.lubycon.curriculum.curriculum.repository.CurriculumRepository;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +12,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class CurriculumService {
 
+  private final CurriculumRepository curriculumRepository;
+
   public List<CurriculumResponse> getCurriculums() {
-    return Collections.EMPTY_LIST;
+    return findAll()
+        .stream()
+        .map(CurriculumResponse::new)
+        .collect(Collectors.toList());
+  }
+
+  private List<Curriculum> findAll() {
+    return curriculumRepository.findAll();
   }
 
 }
