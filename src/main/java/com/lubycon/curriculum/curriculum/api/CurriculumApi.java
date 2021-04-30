bf78dca5ed4e5d7d0f246b2b7fa7ba5d3d3c3726
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-05-01 00:11:53 +0900

    feat: 커리큘럼 리스트 가져오는 컨트롤러 메서드 추가

---
+++ src/main/java/com/lubycon/curriculum/curriculum/api/CurriculumApi.java
@@ -0,0 +1,23 @@
+package com.lubycon.curriculum.curriculum.api;
+
+import com.lubycon.curriculum.curriculum.dto.CurriculumResponse;
+import com.lubycon.curriculum.curriculum.service.CurriculumService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class CurriculumApi {
+
+  private final CurriculumService curriculumService;
+
+  @GetMapping("/curriculums")
+  public ResponseEntity<List<CurriculumResponse>> getAllCurriculums() {
+
+    return ResponseEntity.ok()
+        .body(curriculumService.getCurriculums());
+  }
+}
