1912d2739239c021416945a99fce378d1f63acf8
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-05-01 01:17:38 +0900

    test: 섹션 가져오는 서비스 섹션 PK로 조회하게 수정

--- src/test/resources/make-curriculum.sql
+++ src/test/resources/make-curriculum.sql
@@ -2,9 +2,12 @@ INSERT INTO curriculum (id, title, description, thumbnail) VALUES (1, '1번 커�
 
 INSERT INTO section (id, curriculum_id, order_by, title, description) VALUES (1, 1, 0, '1번 커리큘럼의 섹션1', '1번 커리큘럼의 1번 섹션입니다.')
 INSERT INTO section (id, curriculum_id, order_by, title, description) VALUES (2, 1, 1, '1번 커리큘럼의 섹션2', '1번 커리큘럼의 2번 섹션입니다.')
+INSERT INTO section (id, curriculum_id, order_by, title, description) VALUES (105, 1, 3, '1번 커리큘럼의 섹션4', '1번 커리큘럼의 4번 섹션입니다.')
+INSERT INTO section (id, curriculum_id, order_by, title, description) VALUES (300, 1, 2, '1번 커리큘럼의 섹션3', '1번 커리큘럼의 3번 섹션입니다.')
 
 INSERT INTO blog (id, section_id, title, link) VALUES (1, 1, '1번 섹션의 블로그 제목1', '1번 섹션의 블로그 링크1')
 INSERT INTO blog (id, section_id, title, link) VALUES (2, 1, '1번 섹션의 블로그 제목2', '1번 섹션의 블로그 링크2')
 INSERT INTO blog (id, section_id, title, link) VALUES (3, 2, '2번 섹션의 블로그 제목1', '2번 섹션의 블로그 링크1')
+INSERT INTO blog (id, section_id, title, link) VALUES (4, 300, '3번 섹션의 블로그 제목1', '3번 섹션의 블로그 링크1')
 
 INSERT INTO intro_section (id, curriculum_id, summary, description) VALUES (1, 1, '1번 커리큘럼의 핵심 설명', '1번 커리큘럼의 설명')
