bde48a9ffe0b67b19397e2cb183be066a1b85253
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-05-03 00:40:05 +0900

    test: 섹션 리스트를 가져오는 서비스의 테스트코드 추가

--- src/test/java/com/lubycon/curriculum/curriculum/service/CurriculumServiceTest.java
+++ src/test/java/com/lubycon/curriculum/curriculum/service/CurriculumServiceTest.java
@@ -2,9 +2,14 @@ package com.lubycon.curriculum.curriculum.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.lubycon.curriculum.curriculum.dto.CurriculumInfoResponse;
 import com.lubycon.curriculum.curriculum.dto.CurriculumResponse;
+import com.lubycon.curriculum.curriculum.dto.CurriculumSectionsResponse;
+import com.lubycon.curriculum.curriculum.dto.IntroResponse;
+import com.lubycon.curriculum.curriculum.dto.SectionTitlesResponse;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +37,27 @@ class CurriculumServiceTest {
     assertThat(curriculums.get(index).getTitle()).isEqualTo(index + "번 커리큘럼의 제목");
     assertThat(curriculums.get(index).getDescription()).isEqualTo(index + "번 커리큘럼의 설명");
     assertThat(curriculums.get(index).getThumbnail()).isEqualTo(index + "번 커리큘럼의 썸네일");
+  }
+
+  @Sql("/make-curriculum.sql")
+  @DisplayName("특정 커리큘럼의 정보를 가져온다.")
+  @Test
+  public void getCurriculumsTest() {
+    // when
+    final CurriculumSectionsResponse response = curriculumService.getCurriculumSections(1);
+
+    // then
+    final CurriculumInfoResponse curriculum = response.getCurriculum();
+    assertThat(curriculum.getTitle()).isEqualTo("1번 커리큘럼의 제목");
+
+    final IntroResponse intro = response.getIntro();
+    assertThat(intro.getSummary()).isEqualTo("1번 커리큘럼의 핵심 설명");
+    assertThat(intro.getDescription()).isEqualTo("1번 커리큘럼의 설명");
+
+    final SectionTitlesResponse section = response.getSections().get(2);
+    assertThat(section.getOrder()).isEqualTo(2);
+    assertThat(section.getId()).isEqualTo(300);
+    assertThat(section.getTitle()).isEqualTo("1번 커리큘럼의 섹션3");
   }
 
 }
