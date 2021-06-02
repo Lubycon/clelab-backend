dddd97250d111a3eb88eec725a48ad571cc8b99e
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-06-02 23:48:53 +0900

    feat: TypeformFilter 추가

---
+++ src/main/java/com/lubycon/curriculum/subscribe/filter/TypeformFilter.java
@@ -0,0 +1,36 @@
+package com.lubycon.curriculum.subscribe.filter;
+
+import com.lubycon.curriculum.subscribe.exception.TypeFormSecretNotEquals;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+
+@Slf4j
+@WebFilter(urlPatterns = "/subscribe/typeform")
+public class TypeformFilter implements Filter {
+
+  @Value("${typeform.secret}")
+  private String secretKey;
+
+  @Override
+  public void doFilter(
+      final ServletRequest request, final ServletResponse response, final FilterChain chain)
+      throws IOException, ServletException {
+
+    final HttpServletRequest httpRequest = (HttpServletRequest) request;
+    final String typeFormSignatureValue = httpRequest.getHeader("Typeform-Signature");
+
+    if (!secretKey.equals(typeFormSignatureValue)) {
+      throw new TypeFormSecretNotEquals(typeFormSignatureValue);
+    }
+
+    chain.doFilter(request, response);
+  }
+}
