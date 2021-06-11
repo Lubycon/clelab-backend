826503147e0c5e29b61a03ba8952973682cc7376
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-06-12 04:53:53 +0900

    chore: AOP 의존성 추가, AOP 사용하게 변경

--- src/main/java/com/lubycon/curriculum/CurriculumApplication.java
+++ src/main/java/com/lubycon/curriculum/CurriculumApplication.java
@@ -3,7 +3,9 @@ package com.lubycon.curriculum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @ServletComponentScan
 @SpringBootApplication
 public class CurriculumApplication {
