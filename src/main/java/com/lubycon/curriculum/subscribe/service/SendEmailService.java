252acd8734f34140928e96d4699ef7ab9437a32c
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-06-11 14:17:49 +0900

    feat: 이메일 전송 서비스 추가

---
+++ src/main/java/com/lubycon/curriculum/subscribe/service/SendEmailService.java
@@ -0,0 +1,58 @@
+package com.lubycon.curriculum.subscribe.service;
+
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.SendEmailResult;
+import com.lubycon.curriculum.subscribe.domain.Email;
+import com.lubycon.curriculum.subscribe.dto.EmailSenderDto;
+import com.lubycon.curriculum.subscribe.exception.FailedSendMailException;
+import com.lubycon.curriculum.subscribe.repository.EmailRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SendEmailService {
+
+  private final AmazonSimpleEmailService amazonSimpleEmailService;
+  private final EmailRepository emailRepository;
+
+  public void sendToAllSubscribers(final String subject, final String content) {
+    final List<String> subscribers = emailRepository.findAll()
+        .stream()
+        .map(Email::getEmail)
+        .collect(Collectors.toList());
+
+    send(subject, content, subscribers);
+  }
+
+  public void sendToReceivers(final String subject, final String content,
+      final List<String> receivers) {
+    send(subject, content, receivers);
+  }
+
+  private void send(final String subject, final String content, final List<String> receivers) {
+
+    final EmailSenderDto senderDto = EmailSenderDto.builder()
+        .to(receivers)
+        .subject(subject)
+        .content(content)
+        .build();
+
+    final SendEmailResult sendEmailResult = amazonSimpleEmailService
+        .sendEmail(senderDto.toSendRequestDto());
+
+    sendingResultMustSuccess(sendEmailResult);
+  }
+
+
+  private void sendingResultMustSuccess(final SendEmailResult sendEmailResult) {
+    if (sendEmailResult.getSdkHttpMetadata().getHttpStatusCode() != 200) {
+      throw new FailedSendMailException(sendEmailResult.getSdkResponseMetadata().toString());
+    }
+  }
+
+}
