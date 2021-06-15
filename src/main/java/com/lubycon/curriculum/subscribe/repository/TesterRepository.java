72ad0591793658aa56112c34cdd6f0c914605a0b
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-06-16 00:43:17 +0900

    feat: Tester 엔티티, 레포지토리 추가

---
+++ src/main/java/com/lubycon/curriculum/subscribe/repository/TesterRepository.java
@@ -0,0 +1,8 @@
+package com.lubycon.curriculum.subscribe.repository;
+
+import com.lubycon.curriculum.subscribe.domain.Tester;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TesterRepository extends JpaRepository<Tester, Long> {
+
+}
