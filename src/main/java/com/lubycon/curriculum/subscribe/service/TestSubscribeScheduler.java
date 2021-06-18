9938130e6c7e7e896b20b935773ccb23b5cd6c74
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-06-18 20:06:08 +0900

    fix: Profile 제한 삭제

--- src/main/java/com/lubycon/curriculum/subscribe/service/TestSubscribeScheduler.java
+++ src/main/java/com/lubycon/curriculum/subscribe/service/TestSubscribeScheduler.java
@@ -2,14 +2,12 @@ package com.lubycon.curriculum.subscribe.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
-@Profile("qa")
 @Service
 public class TestSubscribeScheduler {
 
