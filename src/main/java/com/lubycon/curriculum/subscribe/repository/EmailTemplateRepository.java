80b0e98ab7335b24fc1dca29eb97044ac72daa0a
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-06-18 19:26:29 +0900

    chore: 사용하지 않는 메서드 삭제

--- src/main/java/com/lubycon/curriculum/subscribe/repository/EmailTemplateRepository.java
+++ src/main/java/com/lubycon/curriculum/subscribe/repository/EmailTemplateRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface EmailTemplateRepository extends JpaRepository<EmailTemplate, Long> {
 
   Optional<EmailTemplate> findFirstByOrderByCreatedAtDesc();
-
-  Optional<EmailTemplate> findFirstByName();
-
+  
 }
