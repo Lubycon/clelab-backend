587ea1207275ca372c954637df9f77dc3c3abea1
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-06-27 17:08:11 +0900

    fix: 리스트 최신순으로 정렬되게 변경

--- src/main/java/com/lubycon/curriculum/curriculum/repository/CurriculumRepository.java
+++ src/main/java/com/lubycon/curriculum/curriculum/repository/CurriculumRepository.java
@@ -1,8 +1,11 @@
 package com.lubycon.curriculum.curriculum.repository;
 
 import com.lubycon.curriculum.curriculum.domain.Curriculum;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CurriculumRepository extends JpaRepository<Curriculum, Long> {
+
+  List<Curriculum> findByOrderByIdDesc();
 
 }
