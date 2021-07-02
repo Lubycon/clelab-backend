eb5ee9468ed7b36c758c74b7124d1f6a03c8a871
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-07-03 01:46:16 +0900

    test: 구독 취소 서비스 로직 테스트 코드 작성

--- src/main/java/com/lubycon/curriculum/subscribe/repository/EmailRepository.java
+++ src/main/java/com/lubycon/curriculum/subscribe/repository/EmailRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface EmailRepository extends JpaRepository<Email, Long> {
 
   boolean existsByEmail(String email);
+
+  Email findByEmail(String email);
 }
