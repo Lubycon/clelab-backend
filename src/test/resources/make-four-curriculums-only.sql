42695529123eb226ad9c1fda97ea3e367f23225e
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-07-27 00:21:59 +0900

    feat: url_slug 추가

--- src/test/resources/make-four-curriculums-only.sql
+++ src/test/resources/make-four-curriculums-only.sql
@@ -1,4 +1,4 @@
-INSERT INTO curriculum (id, title, description, thumbnail, is_show) VALUES (1, '1번 커리큘럼의 제목', '1번 커리큘럼의 설명', '1번 커리큘럼의 썸네일', 1)
-INSERT INTO curriculum (id, title, description, thumbnail, is_show) VALUES (2, '2번 커리큘럼의 제목', '2번 커리큘럼의 설명', '2번 커리큘럼의 썸네일', 1)
-INSERT INTO curriculum (id, title, description, thumbnail, is_show) VALUES (3, '3번 커리큘럼의 제목', '3번 커리큘럼의 설명', '3번 커리큘럼의 썸네일', 1)
-INSERT INTO curriculum (id, title, description, thumbnail, is_show) VALUES (4, '4번 커리큘럼의 제목', '4번 커리큘럼의 설명', '4번 커리큘럼의 썸네일', 0)
+INSERT INTO curriculum (id, title, description, thumbnail, is_show, url_slug) VALUES (1, '1번 커리큘럼의 제목', '1번 커리큘럼의 설명', '1번 커리큘럼의 썸네일', 1, 'one')
+INSERT INTO curriculum (id, title, description, thumbnail, is_show, url_slug) VALUES (2, '2번 커리큘럼의 제목', '2번 커리큘럼의 설명', '2번 커리큘럼의 썸네일', 1, 'two')
+INSERT INTO curriculum (id, title, description, thumbnail, is_show, url_slug) VALUES (3, '3번 커리큘럼의 제목', '3번 커리큘럼의 설명', '3번 커리큘럼의 썸네일', 1, 'three')
+INSERT INTO curriculum (id, title, description, thumbnail, is_show, url_slug) VALUES (4, '4번 커리큘럼의 제목', '4번 커리큘럼의 설명', '4번 커리큘럼의 썸네일', 0, 'four')
