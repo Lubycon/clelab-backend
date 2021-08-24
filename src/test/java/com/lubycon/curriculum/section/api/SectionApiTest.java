f5449e8a50455ad703c08e9e4d13e8d5c420cf59
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-08-25 00:25:33 +0900

    refactor: deprecated code, 버저닝 관련 코드 제거

--- src/test/java/com/lubycon/curriculum/section/api/SectionApiTest.java
+++ src/test/java/com/lubycon/curriculum/section/api/SectionApiTest.java
@@ -13,63 +13,11 @@ import org.springframework.test.web.servlet.ResultActions;
 class SectionApiTest extends ApiTest {
 
   @Sql("/make-curriculum.sql")
-  @DisplayName("특정 섹션의 내용을 가져온다.")
-  @Test
-  public void getSectionTest() throws Exception {
-    // given
-    final String url = "/v1/curriculums/{curriculumId}/sections/{sectionId}";
-
-    // when
-    final ResultActions resultActions = mockMvc.perform(get(url, 1, 300));
-
-    // then
-    resultActions
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.title").value("1번 커리큘럼의 섹션3"))
-        .andExpect(jsonPath("$.order").value(2))
-        .andExpect(jsonPath("$.description").value("1번 커리큘럼의 3번 섹션입니다."))
-        .andExpect(jsonPath("$.blogs[0].title").value("3번 섹션의 블로그 제목1"))
-        .andExpect(jsonPath("$.nextSection.title").value("1번 커리큘럼의 섹션4"))
-        .andExpect(jsonPath("$.prevSection.title").value("1번 커리큘럼의 섹션2"));
-  }
-
-  @Sql("/make-curriculum.sql")
-  @DisplayName("특정 커리큘럼의 섹션들을 가져온다.")
-  @Test
-  public void getAllSections() throws Exception {
-    // given
-    final String url = "/v2/curriculums/{curriculumId}/sections";
-
-    // when
-    final ResultActions resultActions = mockMvc.perform(get(url, 1));
-
-    // then
-    resultActions
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.curriculum.title").value("1번 커리큘럼의 제목"))
-        .andExpect(jsonPath("$.intro.description.summary").value("1번 커리큘럼의 핵심 설명"))
-        .andExpect(jsonPath("$.intro.description.subSummary").value("1번 커리큘럼의 핵심 설명2222"))
-        .andExpect(jsonPath("$.intro.majorCompany.title").value("메이저 회사는 React를 얼마나 사용하고 있을까요?"))
-        .andExpect(jsonPath("$.intro.majorCompany.companies.naver").value(true))
-        .andExpect(jsonPath("$.intro.majorCompany.companies.kakao").value(false))
-        .andExpect(jsonPath("$.intro.googleTrend.csvHtml").value("<p>안녕하세요</p>"))
-        .andExpect(jsonPath("$.intro.stackOverflowTrend.title").value("스택오버플로우 트렌드"))
-        .andExpect(jsonPath("$.intro.stackOverflowTrend.imagePath").value("스택오버플로우 이미지링크"))
-        .andExpect(jsonPath("$.intro.statistics[0].title").value("관련 Github 레포지토리 수"))
-        .andExpect(jsonPath("$.intro.statistics[0].values[0].keyword").value("React"))
-        .andExpect(jsonPath("$.intro.statistics[0].values[0].value").value("220만"))
-        .andExpect(jsonPath("$.sections[2].order").value(2))
-        .andExpect(jsonPath("$.sections[2].id").value(300))
-        .andExpect(jsonPath("$.sections[2].title").value("1번 커리큘럼의 섹션3"));
-  }
-
-
-  @Sql("/make-curriculum.sql")
   @DisplayName("slug를 사용해서 특정 커리큘럼의 섹션들을 가져온다.")
   @Test
   public void getAllSectionsFromSlug() throws Exception {
     // given
-    final String url = "/v2/courses/{courseSlug}/sections";
+    final String url = "/courses/{courseSlug}/sections";
 
     // when
     final ResultActions resultActions = mockMvc.perform(get(url, "curriculum-1"));
@@ -95,47 +43,11 @@ class SectionApiTest extends ApiTest {
   }
 
   @Sql("/make-curriculum.sql")
-  @DisplayName("특정 커리큘럼이 없다면 404가 발생한다.")
-  @Test
-  public void notFoundCurriculumTest() throws Exception {
-    // given
-    final String url = "/v1/curriculums/{curriculumId}/sections";
-    final long notExistId = 1222;
-
-    // when
-    final ResultActions resultActions = mockMvc.perform(get(url, notExistId));
-
-    // then
-    resultActions
-        .andExpect(status().isNotFound())
-        .andExpect(jsonPath("$.message").value(notExistId + "에 해당하는 커리큘럼이 없습니다."))
-        .andExpect(jsonPath("$.code").value("C005"));
-  }
-
-  @Sql("/make-curriculum.sql")
-  @DisplayName("특정 커리큘럼이 없다면 404가 발생한다.")
-  @Test
-  public void notFoundSectionTest() throws Exception {
-    // given
-    final String url = "/v1/curriculums/{curriculumId}/sections/{sectionId}";
-    final long notExistId = 1222;
-
-    // when
-    final ResultActions resultActions = mockMvc.perform(get(url, 1, notExistId));
-
-    // then
-    resultActions
-        .andExpect(status().isNotFound())
-        .andExpect(jsonPath("$.message").value(notExistId + "에 해당하는 섹션이 없습니다."))
-        .andExpect(jsonPath("$.code").value("C005"));
-  }
-
-  @Sql("/make-curriculum.sql")
   @DisplayName("특정 섹션의 내용을 slug를 통해 가져온다.")
   @Test
   public void getSectionBySlugTest() throws Exception {
     // given
-    final String url = "/v2/courses/{curriculumSlug}/sections/{sectionSlug}";
+    final String url = "/courses/{curriculumSlug}/sections/{sectionSlug}";
 
     // when
     final ResultActions resultActions = mockMvc.perform(get(url, "curriculum-1", "three"));
