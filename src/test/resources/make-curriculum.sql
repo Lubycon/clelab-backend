c417483420e7476693aba85da5f3f15a899a299c
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-08-30 00:09:32 +0900

    test: Blog 정렬순으로 가져오게 처리

--- src/test/resources/make-curriculum.sql
+++ src/test/resources/make-curriculum.sql
@@ -5,11 +5,12 @@ INSERT INTO section (id, curriculum_id, order_by, title, description, url_slug) 
 INSERT INTO section (id, curriculum_id, order_by, title, description, url_slug) VALUES (105, 1, 3, '1번 커리큘럼의 섹션4', '1번 커리큘럼의 4번 섹션입니다.', 'four')
 INSERT INTO section (id, curriculum_id, order_by, title, description, url_slug) VALUES (300, 1, 2, '1번 커리큘럼의 섹션3', '1번 커리큘럼의 3번 섹션입니다.', 'three')
 
-INSERT INTO blog (id, section_id, title, link, clelab_pick) VALUES (1, 1, '1번 섹션의 블로그 제목1', '1번 섹션의 블로그 링크1', 0)
-INSERT INTO blog (id, section_id, title, link, clelab_pick) VALUES (2, 1, '1번 섹션의 블로그 제목2', '1번 섹션의 블로그 링크2', 0)
-INSERT INTO blog (id, section_id, title, link, clelab_pick) VALUES (3, 2, '2번 섹션의 블로그 제목1', '2번 섹션의 블로그 링크1', 0)
-INSERT INTO blog (id, section_id, title, link, clelab_pick) VALUES (4, 300, '3번 섹션의 블로그 제목1', '3번 섹션의 블로그 링크1', 0)
-INSERT INTO blog (id, section_id, title, link, clelab_pick) VALUES (5, 300, '3번 섹션의 블로그 제목1', '3번 섹션의 블로그 링크2', 1)
+INSERT INTO blog (id, section_id, title, link, clelab_pick, order_by) VALUES (1, 1, '1번 섹션의 블로그 제목1', '1번 섹션의 블로그 링크1', 0, 1)
+INSERT INTO blog (id, section_id, title, link, clelab_pick, order_by) VALUES (2, 1, '1번 섹션의 블로그 제목2', '1번 섹션의 블로그 링크2', 0, 2)
+INSERT INTO blog (id, section_id, title, link, clelab_pick, order_by) VALUES (3, 2, '2번 섹션의 블로그 제목1', '2번 섹션의 블로그 링크1', 0, 1)
+INSERT INTO blog (id, section_id, title, link, clelab_pick, order_by) VALUES (4, 300, '3번 섹션의 블로그 제목1', '3번 섹션의 블로그 링크1', 0, 1)
+INSERT INTO blog (id, section_id, title, link, clelab_pick, order_by) VALUES (5, 300, '3번 섹션의 블로그 제목3', '3번 섹션의 블로그 링크3', 1, 3)
+INSERT INTO blog (id, section_id, title, link, clelab_pick, order_by) VALUES (6, 300, '3번 섹션의 블로그 제목2', '3번 섹션의 블로그 링크2', 1, 2)
 
 INSERT INTO major_company_frequency (id, title, naver, kakao, line, coupang, woowabros, toss, daangn, yanolja) VALUES (0, '메이저 회사는 React를 얼마나 사용하고 있을까요?', true, false, true, true, true, false, true, true)
 
