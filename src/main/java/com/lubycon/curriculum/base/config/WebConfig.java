56c6565a540de76533fd3b54d14cc97e2d48bf6a
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-09-06 00:33:19 +0900

    fix: cors all open

--- src/main/java/com/lubycon/curriculum/base/config/WebConfig.java
+++ src/main/java/com/lubycon/curriculum/base/config/WebConfig.java
@@ -10,8 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(final CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://clelab.io",
-            "https://qa.clelab.io", "https://*.clelab.io", "https://www.clelab.io/",
-            "https://www.qa.clelab.io/");
+        .allowedOrigins("*");
   }
 }
